5386642125e41e1f325fcf5c3b96c111a01ba1d3
Alfian Nurrizky <alfiannrzky0@gmail.com>
2023-05-14 13:47:04 +0700

    create a model user and migration

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -1,10 +1,12 @@
-{
-  "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+require('dotenv').config()
+
+module.exports = {
+  "development" : {
+    "username"  : process.env.DB_USERNAME,
+    "password"  : process.env.DB_PASSWORD,
+    "database"  : process.env.DB_NAME,
+    "host"      : process.env.DB_HOST,
+    "dialect"   : process.env.DIALECT
   },
   "test": {
     "username": "root",
--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ const app = express()
 const cors = require('cors')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
+const router = require('./routes/index')
 
 require('dotenv').config()
 
@@ -11,5 +12,6 @@ app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(morgan('combined'))
 app.use(cors())
+app.use(router)
 
 app.listen(process.env.PORT, () => {console.log(`server running on port ${process.env.PORT}`)})
